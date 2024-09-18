c7a0b543a7468ad26d67a23c0eff968c76deefaf
DINESH-KUMAR7159 <18euit032@skcet.ac.in>
2024-09-19 00:49:21 +0530

    Exceptions added and swagger dependency added

--- src/main/java/com/ecommerce/productservice/controllers/ProductController.java
+++ src/main/java/com/ecommerce/productservice/controllers/ProductController.java
@@ -5,6 +5,8 @@ import com.ecommerce.productservice.models.Product;
 import com.ecommerce.productservice.service.ProductService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/products")
 public class ProductController {
@@ -14,8 +16,13 @@ public class ProductController {
     }
 
     @GetMapping("/{id}")
-    public @ResponseBody  GetProductDto getProductById(@PathVariable("id") Long id) {
+    public @ResponseBody  GetProductDto getProductById(@PathVariable("id") Long id) throws Exception{
         return productService.getProductById(id);
+    }
+
+    @GetMapping("")
+    public @ResponseBody List<GetProductDto> getAllProducts() {
+        return productService.getAllProducts();
     }
 
     @PostMapping("")
--- src/main/java/com/ecommerce/productservice/service/ProductService.java
+++ src/main/java/com/ecommerce/productservice/service/ProductService.java
@@ -1,27 +1,59 @@
 package com.ecommerce.productservice.service;
 
 import com.ecommerce.productservice.dtos.GetProductDto;
+import com.ecommerce.productservice.exceptions.NotFoundException;
 import com.ecommerce.productservice.models.Product;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ProductService {
 
 
-
-    public GetProductDto getProductById(Long id) {
+    public GetProductDto getProductById(Long id) throws NotFoundException {
 
         RestTemplate restTemplate = new RestTemplate();
 
         String url = "https://fakestoreapi.com/products/" + id;
         Product product = restTemplate.getForObject(url, Product.class);
         System.out.println(product);
+
+        if(product == null){
+            throw new NotFoundException();
+
+        }
+//        GetProductDto obj = new GetProductDto();
+//        obj.setName(product.getTitle());
+//        obj.setPrice(product.getPrice());
+//        obj.setImageUrl(product.getImage());
+        return convertProductToProductDto(product);
+    }
+
+    private static GetProductDto convertProductToProductDto(Product product) {
         GetProductDto obj = new GetProductDto();
         obj.setName(product.getTitle());
         obj.setPrice(product.getPrice());
         obj.setImageUrl(product.getImage());
-  return obj;
+        return obj;
     }
+
+    public List<GetProductDto> getAllProducts() {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://fakestoreapi.com/products";
+
+        Product[] product = restTemplate.getForObject(url, Product[].class);
+
+        List<GetProductDto> returnProducts = new ArrayList<>();
+        for(Product products : product) {
+            returnProducts.add(convertProductToProductDto(products));
+        }
+
+        return returnProducts;
+
+    }
+
 
 }
